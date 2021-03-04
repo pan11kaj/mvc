095da0db3e28c6d43bc7a4cdee8924049d1fd1b7
pan11kaj <68064022+pan11kaj@users.noreply.github.com>
2021-03-04 15:11:02 +0530

    Add files via upload

---
+++ apirunning.py
@@ -0,0 +1,12 @@
+from classifier import get_prediction;
+from flask import Flask;
+app = Flask(__name__)
+@app.route("/predict-alphabet", methods=["POST"])
+def predict_data():
+    image = request.files.get("alphabet")
+    prediction = get_prediction(image)
+    return jsonify({
+    "prediction": prediction
+    }), 200
+if __name__ == "__main__":
+  app.run(debug=True)
